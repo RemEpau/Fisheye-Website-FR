21eae6282fa3e4082c78bb65e5eec9d549886379
Rémi Épaulais <epaulais.remi@gmail.com>
2024-01-04 12:32:41 +0100

    Fetching data + display

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,46 +1,32 @@
-    async function getPhotographers() {
-        // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
-        // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
-        // et bien retourner le tableau photographers seulement une fois récupéré
-        return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
-    }
+async function getPhotographers() {
+    // Récupérer les données des photographes en utilisant fetch depuis le fichier json
 
-    async function displayData(photographers) {
-        const photographersSection = document.querySelector(".photographer_section");
+    let photographers = await fetch("../../data/photographers.json")
+        .then((response) => response.json())
 
-        photographers.forEach((photographer) => {
-            const photographerModel = photographerTemplate(photographer);
-            const userCardDOM = photographerModel.getUserCardDOM();
-            photographersSection.appendChild(userCardDOM);
-        });
-    }
+    // Ensuite, retourner les données
 
-    async function init() {
-        // Récupère les datas des photographes
-        const { photographers } = await getPhotographers();
-        displayData(photographers);
-    }
-    
-    init();
-    
+    return ({
+        photographers: photographers['photographers']
+    });
+}
+
+async function displayData(photographers) {
+    const photographersSection = document.querySelector(".photographer_section");
+
+    photographers.forEach((photographer) => {
+        // console.log(photographer);
+        const photographerModel = photographerTemplate(photographer);
+        const userCardDOM = photographerModel.getUserCardDOM();
+        photographersSection.appendChild(userCardDOM);
+    });
+}
+
+async function init() {
+    // Récupère les datas des photographes
+    const { photographers } = await getPhotographers();
+    displayData(photographers);
+}
+
+init();
+
--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,17 +1,38 @@
+/**
+ * Génère un modèle de photographe basé sur les données fournies.
+ *
+ * @param {Object} data - L'objet de données contenant les informations du photographe.
+ * @param {string} data.name - Le nom du photographe.
+ * @param {string} data.portrait - Le chemin vers l'image du portrait du photographe.
+ * @returns {Object} - Un objet contenant le nom du photographe, l'image et une fonction pour générer le DOM de la carte utilisateur.
+ */
 function photographerTemplate(data) {
     const { name, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = portrait ? `assets/photographers/${portrait}` : 'assets/photographers/account.png'
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        // Elements à créer
+        const article = document.createElement('article');
+        const container = document.createElement('div');
+        const img = document.createElement('img');
+        const h2 = document.createElement('h2');
+
+        // Ajout des classes et attributs
+        container.classList.add('container');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        img.setAttribute("alt", `Portrait de ${name}`)
+
+        // Ajout du contenu
         h2.textContent = name;
-        article.appendChild(img);
+        article.appendChild(container);
+        container.appendChild(img);
         article.appendChild(h2);
+
+        // Retourner le DOM
         return (article);
     }
+
+    // Retourner la fonction getUserCardDOM et les données name et picture
     return { name, picture, getUserCardDOM }
 }
