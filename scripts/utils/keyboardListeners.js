858fc5dc144c3611bf89f7dd7b82099546c26472
Rémi Épaulais <epaulais.remi@gmail.com>
2024-02-13 09:24:47 +0100

    Removed unnecessary EventListener on Enter

--- scripts/utils/keyboardListeners.js
+++ scripts/utils/keyboardListeners.js
@@ -10,16 +10,6 @@ export function escapeListener(callback) {
     });
 }
 
-export function enterListener(callback) {
-    document.addEventListener("keydown", function (e) {
-        // Si la touche "Enter" est pressée, on appelle la fonction callback et annule le comportement par défaut
-        if (e.code === "Enter") {
-            callback();
-            e.preventDefault();
-        }
-    });
-}
-
 export function arrowListener(callbackLeft, callbackRight) {
     document.addEventListener("keydown", function (e) {
         if (e.code === "ArrowLeft") {
